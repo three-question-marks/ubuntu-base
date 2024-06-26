4f38b9c11d57f8e69ae1a55f369a0efb36a7d0a0
three-question-marks <three-question-marks@users.noreply.github.com>
2024-06-26 10:15:08 +0300

    fix: add locales package for cloud-init

--- scripts/30_chroot/90_cloud-init.sh
+++ scripts/30_chroot/90_cloud-init.sh
@@ -3,5 +3,5 @@ set -eu
 
 if [ "${PKR_INSTALL_CLOUD_INIT}" = 'true' ]; then
     echo \> Installing cloud-init
-    apt-get -y install --no-install-recommends cloud-init
+    apt-get -y install --no-install-recommends cloud-init locales
 fi
